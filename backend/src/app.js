1c8c6d95d08890243fe6f12c1f729ce538bb1f6b
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-07 17:45:52 +0530

    in GoogleStrategy change callbackURL

--- backend/src/app.js
+++ backend/src/app.js
@@ -73,11 +73,11 @@ passport.deserializeUser(User.deserializeUser());
 
 
 passport.use(new GoogleStrategy({
-  clientID:process.env.GOOGLE_CLIENT_ID,
-  clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
-
-},async (accessToken, refreshToken, profile, done) => {
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:5000/auth/google/callback"
+},
+async (accessToken, refreshToken, profile, done) => {
   try {
     const existingUser = await User.findOne({ providerId: profile.id });
 
@@ -85,7 +85,6 @@ passport.use(new GoogleStrategy({
       return done(null, existingUser);
     }
 
-    // Create new user if not exists
     const newUser = new User({
       email: profile.emails[0].value,
       provider: "google",
@@ -97,7 +96,8 @@ passport.use(new GoogleStrategy({
   } catch (err) {
     done(err, null);
   }
-}))
+}));
+
 
 
 
