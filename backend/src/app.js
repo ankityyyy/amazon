97cb6636d3f5c65432e027d26ee5b5d1d10b5a66
ankityyyy <159771196+ankityyyy@users.noreply.github.com>
2025-07-24 20:32:48 +0530

    add live url of backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -40,7 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://amazon-frontend-c2ki.onrender.com","http://localhost:5173",],
     credentials: true, 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   })
